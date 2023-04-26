c9851a2849661262b7c8641a175aec1930062fcc
eugene-hanada <76219486+eugene-hanada@users.noreply.github.com>
2023-04-26 11:34:25 +0900

    頂点、インデック、テクスチャパスをファイルに出力してみる

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <memory>
 #include <deque>
+#include <fstream>
 #include <EugeneLib.h>
 
 #include "EugeneLib/Include/Graphics/IndexView.h"
@@ -14,13 +15,43 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "tinygltf/tiny_gltf.h"
 
+/// <summary>
+/// 頂点
+/// </summary>
 struct GltfVertex
 {
 	Eugene::Vector3 pos;
 	Eugene::Vector3 normal;
+	Eugene::Vector3 tan;
 	Eugene::Vector2 uv;
+};
+
+/// <summary>
+/// アニメーションあり頂点
+/// </summary>
+struct SkeletalGltfVertex
+{
+	Eugene::Vector3 pos;
+	Eugene::Vector3 normal;
+	Eugene::Vector2 uv;
+	Eugene::Vector3 tan;
 	std::uint16_t joint[4];
 	float weight[4];
+};
+
+struct StaticModel
+{
+	char sig[4]{ 's','m','d','l' };
+	float version;
+	std::uint32_t meshNum;
+	std::uint32_t textureNum;
+	std::uint32_t materialNum;
+};
+
+struct Material
+{
+	std::uint32_t baseColorTexture;
+	std::uint32_t normalMapTexture;
 };
 
 struct Camera
@@ -32,7 +63,7 @@ struct Camera
 struct Mesh
 {
 	Mesh(){}
-	Mesh(Mesh&& mesh)
+	Mesh(Mesh&& mesh) noexcept
 	{
 		vertex = std::move(mesh.vertex);
 		index = std::move(mesh.index);
@@ -42,7 +73,7 @@ struct Mesh
 		indexView = std::move(mesh.indexView);
 	}
 
-	Mesh& operator=(Mesh&& mesh)
+	Mesh& operator=(Mesh&& mesh) noexcept
 	{
 		vertex = std::move(mesh.vertex);
 		index = std::move(mesh.index);
@@ -67,8 +98,39 @@ void LoadGltf(std::list<Mesh>& list, const std::string& path)
 	std::string err;
 	std::string warn;
 	gltf.LoadASCIIFromFile(&model, &err, &warn, path);
-
 	std::vector<std::uint8_t> joints;
+
+	StaticModel h{};
+	std::ofstream file{ "./" + path.substr(0, path.find_last_of(".")) + ".smdl", std::ios::binary };
+	h.version = 0.01f;
+	for (auto& mesh : model.meshes)
+	{
+		h.meshNum += static_cast<std::uint32_t>(mesh.primitives.size());
+	}
+	
+	h.textureNum = static_cast<std::uint32_t>(model.images.size());
+	h.materialNum = static_cast<std::uint32_t>(model.materials.size());
+
+	file.write(reinterpret_cast<char*>(&h), sizeof(h));
+
+	// 画像情報
+	for (auto& img : model.images)
+	{
+		auto size = img.name.size();
+		file.write(reinterpret_cast<char*>(&size), sizeof(size));
+		file.write(img.name.data(), sizeof(img.name[0]) * size);
+
+		//stbi_write_png(("./" + img.name + ".png").c_str(), img.width, img.height, img.component, img.image.data(), 0);
+	}
+
+	Material m{};
+	for (auto& material : model.materials)
+	{
+		m.baseColorTexture = material.pbrMetallicRoughness.baseColorTexture.index;
+		m.normalMapTexture = material.normalTexture.index;
+		file.write(reinterpret_cast<char*>(&m), sizeof(m));
+	}
+
 
 	for (auto& mesh : model.meshes)
 	{
@@ -76,38 +138,45 @@ void LoadGltf(std::list<Mesh>& list, const std::string& path)
 		{
 			auto& posAccsessor = model.accessors[primitive.attributes["POSITION"]];
 			auto& normAccessor = model.accessors[primitive.attributes["NORMAL"]];
+			auto& tanAccessor = model.accessors[primitive.attributes["TANGENT"]];
 			auto& uvAccessor = model.accessors[primitive.attributes["TEXCOORD_0"]];
-			auto& boneAccessor = model.accessors[primitive.attributes["JOINTS_0"]];
-			auto& weighisAccessor = model.accessors[primitive.attributes["WEIGHIS_0"]];
+			
+
+			//auto& boneAccessor = model.accessors[primitive.attributes["JOINTS_0"]];
+			//auto& weighisAccessor = model.accessors[primitive.attributes["WEIGHIS_0"]];
 
 			auto& posBufferView = model.bufferViews[posAccsessor.bufferView];
 			auto& normBufferView = model.bufferViews[normAccessor.bufferView];
+			auto& tanBufferView = model.bufferViews[tanAccessor.bufferView];
 			auto& uvBufferView = model.bufferViews[uvAccessor.bufferView];
-			auto& boneBufferView = model.bufferViews[boneAccessor.bufferView];
-			auto& weighisBufferView = model.bufferViews[weighisAccessor.bufferView];
+			//auto& boneBufferView = model.bufferViews[boneAccessor.bufferView];
+			//auto& weighisBufferView = model.bufferViews[weighisAccessor.bufferView];
 
 			auto& posBuffer = model.buffers[posBufferView.buffer];
 			auto& normBuffer = model.buffers[normBufferView.buffer];
+			auto& tanBuffer = model.buffers[tanBufferView.buffer];
 			auto& uvBuffer = model.buffers[uvBufferView.buffer];
-			auto& boneBuffer = model.buffers[boneBufferView.buffer];
-			auto& weighisBuffer = model.buffers[weighisBufferView.buffer];
+			//auto& boneBuffer = model.buffers[boneBufferView.buffer];
+			//auto& weighisBuffer = model.buffers[weighisBufferView.buffer];
 
 			auto pos = reinterpret_cast<float*>(&posBuffer.data[posBufferView.byteOffset + posAccsessor.byteOffset]);
 			auto norm = reinterpret_cast<float*>(&normBuffer.data[normBufferView.byteOffset + normAccessor.byteOffset]);
+			auto tan = reinterpret_cast<float*>(&tanBuffer.data[tanBufferView.byteOffset + tanAccessor.byteOffset]);
 			auto uv = reinterpret_cast<float*>(&uvBuffer.data[uvBufferView.byteOffset + uvAccessor.byteOffset]);
-			auto joint = reinterpret_cast<std::uint8_t*>(&boneBuffer.data[boneBufferView.byteOffset + boneAccessor.byteOffset]);
-			auto weight = reinterpret_cast<float*>(&weighisBuffer.data[weighisBufferView.byteOffset + weighisAccessor.byteOffset]);
+			//auto joint = reinterpret_cast<std::uint8_t*>(&boneBuffer.data[boneBufferView.byteOffset + boneAccessor.byteOffset]);
+			//auto weight = reinterpret_cast<float*>(&weighisBuffer.data[weighisBufferView.byteOffset + weighisAccessor.byteOffset]);
 
 			Mesh mesh;
 
 			mesh.vertex.resize(posAccsessor.count);
 			for (int i = 0; i < posAccsessor.count; i++)
 			{
-				std::uint8_t j[4];
+				//std::uint8_t j[4];
 				mesh.vertex[i].pos = Eugene::Vector3{ pos[i * 3 + 0],pos[i * 3 + 1],pos[i * 3 + 2] };
 				mesh.vertex[i].normal = Eugene::Vector3{ norm[i * 3 + 0],norm[i * 3 + 1],norm[i * 3 + 2] };
+				mesh.vertex[i].tan = Eugene::Vector3{ tan[i * 3 + 0],tan[i * 3 + 1],tan[i * 3 + 2] };
 				mesh.vertex[i].uv = Eugene::Vector2{ uv[i * 2 + 0],uv[i * 2 + 1] };
-				mesh.vertex[i].joint[0] = joint[i * 4 + 0];
+				/*mesh.vertex[i].joint[0] = joint[i * 4 + 0];
 				mesh.vertex[i].joint[1] = joint[i * 4 + 1];
 				mesh.vertex[i].joint[2] = joint[i * 4 + 2];
 				mesh.vertex[i].joint[3] = joint[i * 4 + 3];
@@ -115,7 +184,7 @@ void LoadGltf(std::list<Mesh>& list, const std::string& path)
 				mesh.vertex[i].weight[0] = weight[i * 4 + 0];
 				mesh.vertex[i].weight[1] = weight[i * 4 + 1];
 				mesh.vertex[i].weight[2] = weight[i * 4 + 2];
-				mesh.vertex[i].weight[3] = weight[i * 4 + 3];
+				mesh.vertex[i].weight[3] = weight[i * 4 + 3];*/
 			}
 
 			auto& accessor = model.accessors[primitive.indices];
@@ -127,9 +196,25 @@ void LoadGltf(std::list<Mesh>& list, const std::string& path)
 			{
 				mesh.index[i] = idxP[i];
 			}
+
+
+			// メッシュ情報
+			
+			auto size = mesh.vertex.size();
+			file.write(reinterpret_cast<char*>(&size), sizeof(size));
+			file.write(reinterpret_cast<char*>(mesh.vertex.data()), sizeof(mesh.vertex[0]) * mesh.vertex.size());
+
+			size = mesh.index.size();
+			file.write(reinterpret_cast<char*>(&size), sizeof(size));
+			file.write(reinterpret_cast<char*>(mesh.index.data()), sizeof(mesh.index[0]) * mesh.index.size());
+			
+			std::uint64_t materialIdx = primitive.material;
+			file.write(reinterpret_cast<char*>(&materialIdx), sizeof(materialIdx));
+
 			list.emplace_back(std::move(mesh));
 		}
 	}
+
 }
 
 void MeshInit(Eugene::Graphics& graphics, Mesh& mesh)
@@ -149,7 +234,7 @@ void MeshInit(Eugene::Graphics& graphics, Mesh& mesh)
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
 	std::unique_ptr<Eugene::System> system;
 	system.reset(Eugene::CreateSystem({ 1280.0f,720.0f }, u8"モデルテスト"));
@@ -170,9 +255,7 @@ int main()
 	{
 		{ "POSITION", 0, Eugene::Format::R32G32B32_FLOAT },
 		{ "NORMAL", 0, Eugene::Format::R32G32B32_FLOAT },
-		{ "TEXCOORD", 0, Eugene::Format::R32G32_FLOAT },
-		{ "BONE_NO", 0, Eugene::Format::R16G16B16A16_UINT},
-		{ "WEIGHT", 0, Eugene::Format::R32G32B32A32_FLOAT}
+		{ "TEXCOORD", 0, Eugene::Format::R32G32_FLOAT }
 	};
 
 	// シェーダー
@@ -193,24 +276,15 @@ int main()
 		{ Eugene::ShaderLayout{ Eugene::ViewType::ConstantBuffer, 1,0 } }
 	};
 
-	try
-	{
-		pipeline.reset(
-			graphics->CreateGraphicsPipeline(
-				layout,
-				shaders,
-				rendertargets,
-				Eugene::TopologyType::Triangle,
-				false, false,
-				shaderLayout
-			));
-	}
-	catch (const std::exception& t)
-	{
-		return 0;
-	}
-
-	
+	pipeline.reset(
+		graphics->CreateGraphicsPipeline(
+			layout,
+			shaders,
+			rendertargets,
+			Eugene::TopologyType::Triangle,
+			false, false,
+			shaderLayout
+		));
 
 
 	std::vector<GltfVertex> vertex_;
@@ -220,23 +294,6 @@ int main()
 
 	LoadGltf(meshList, "Swat.gltf");
 
-	//// 頂点バッファとビュー作成
-	//std::unique_ptr<Eugene::BufferResource> vertexBuffer;
-	//std::unique_ptr<Eugene::VertexView> vertexView;
-	//vertexBuffer.reset(graphics->CreateUploadableBufferResource(sizeof(GltfVertex) * vertex_.size()));
-	//vertexView.reset(graphics->CreateVertexView(sizeof(GltfVertex), vertex_.size(), *vertexBuffer));
-	//GltfVertex* mapped = reinterpret_cast<GltfVertex*>(vertexBuffer->Map());
-	//std::copy(vertex_.begin(), vertex_.end(), mapped);
-	//vertexBuffer->UnMap();
-
-	//// インデックスバッファとビュー作成
-	//std::unique_ptr<Eugene::BufferResource> indexBuffer;
-	//std::unique_ptr<Eugene::IndexView> indexView;
-	//indexBuffer.reset(graphics->CreateUploadableBufferResource(sizeof(index[0]) * index.size()));
-	//indexView.reset(graphics->CreateIndexView(sizeof(index[0]) * index.size(), Eugene::Format::R16_UINT, *indexBuffer));
-	//std::uint16_t* indexMap = reinterpret_cast<std::uint16_t*>(indexBuffer->Map());
-	//std::copy(index.begin(), index.end(), indexMap);
-	//indexBuffer->UnMap();
 	for (auto& mesh : meshList)
 	{
 		MeshInit(*graphics, mesh);
